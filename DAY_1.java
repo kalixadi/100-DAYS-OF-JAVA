3cc652105aed74f1048a86689284966c0bb8f472
Aditya Siddharth Jyoti <112400713+kalixadi@users.noreply.github.com>
2023-07-17 11:27:21 +0530

    Day_1
    1st day of java coding , learnt about println and print , learnt about structure of java codelines .

---
+++ DAY_1.java
@@ -0,0 +1,13 @@
+package com.company;
+public class DAY_1 {
+    public static void main(String[] args) {
+
+        System.out.println("hello world"); // this println is used to print an aditional new line character after the execution of this line
+        int num1 = 18;
+        int num2 = 19;
+        int num3 = 20;
+        int sum = num1 + num2 + num3;
+        System.out.print("The sum of the numbers is : ");
+        System.out.println(sum);
+    }
+}
